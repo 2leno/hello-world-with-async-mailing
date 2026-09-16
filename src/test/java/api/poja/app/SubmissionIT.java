ad7679257c407fd4d808d5269262729ec177bc71
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-09-16 09:55:58 +0300

    fix: correct MockBean import path

--- src/test/java/api/poja/app/SubmissionIT.java
+++ src/test/java/api/poja/app/SubmissionIT.java
@@ -14,7 +14,7 @@ import java.util.List;
 import javax.imageio.ImageIO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
@@ -28,9 +28,9 @@ public class SubmissionIT extends FacadeIT {
 
   @Autowired private TestRestTemplate restTemplate;
 
-  @MockitoBean private BucketComponent bucketComponent;
+  @MockBean private BucketComponent bucketComponent;
 
-  @MockitoBean private Mailer mailer;
+  @MockBean private Mailer mailer;
 
   private static final String BASE_URL = "/submissions";
 
